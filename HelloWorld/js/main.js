e5d4bb8e4e2a2059ddb5c1668d8092fd36f8926c
yinchunxiang <yinchunxiang@Kwai.local>
2019-04-12 14:05:59 +0800

    up

--- HelloWorld/js/main.js
+++ HelloWorld/js/main.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
+    // 让header填满整个窗口
     $('.header').height($(window).height());
+
+    // 控制跳转，其实就是个滑动
+    $(".navbar a").click(function(){
+        console.log("-->" + $(this).data('value'))
+        // $("body,html").animate({
+        $([document.documentElement, document.body]).animate({
+            scrollTop:$("#" + $(this).data('value')).offset().top
+        },1000)
+
+    })
 })
